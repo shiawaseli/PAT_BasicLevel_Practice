563012f055830d20c09632d56cd4db26daf1e1ac
shiawaseli <shiawaseli@users.noreply.github.com>
2019-05-10 21:08:04 +0800

    practice09b

--- practice09/practice09/1088_三人行.c
+++ practice09/practice09/1088_三人行.c
@@ -3,48 +3,49 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-int main()
+void Print(double a, double M)
 {
-	int i, a, b, c, M, X, Y, tmp, abc[3] = { 0 };
+	if (a > M)
+	{
+		printf(" Cong");
+	}
+	else if (a == M)
+	{
+		printf(" Ping");
+	}
+	else
+	{
+		printf(" Gai");
+	}
+}
+
+int main8()
+{
+	int a, b, M, X, Y;
+	double c;
 
 	scanf("%d%d%d", &M, &X, &Y);
-	for (c = 0; c <= 1000; c++)
+	for (a = 99; a >= 10; a--)
 	{
-		b = Y * c;
-		a = X * c + b;
-		if (a >= 100)
+		b = a % 10 * 10 + a / 10;
+		c = abs(a - b) * 1.0 / X;
+		if (b == Y * c)
 		{
-			tmp = a / 100 + a / 10 % 10 * 10 + a % 10 * 100;
-		}
-		else
-		{
-			tmp = a / 10 + a % 10 * 10;
-		}
-		if (tmp == b)
-		{
-			abc[0] = a;
-			abc[1] = b;
-			abc[2] = c;
+			printf("%d", a);
+			Print(a, M);
+			Print(b, M);
+			Print(c, M);
+			break;
 		}
 	}
-	printf("%d", abc[0]);
-	for (i = 0; i < 3; i++)
+	if (a < 10)
 	{
-		if (abc[i] > M)
-		{
-			printf(" Cong");
-		}
-		else if (abc[i] == M)
-		{
-			printf(" Ping");
-		}
-		else
-		{
-			printf(" Gai");
-		}
+		printf("No Solution");
 	}
 	putchar('\n');
 
 	system("pause");
 	return 0;
 }
+
+//注意丙可能不是整数
